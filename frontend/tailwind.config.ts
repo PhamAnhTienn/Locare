60b8dc448882afe6237039510afd3611a9f8d7ff
Pham Anh Tien <anhtien13082005@gmail.com>
2024-09-21 17:10:42 +0700

    setup added

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      'locare': '#ff385c',
+      'locare-dark': '#d50027'
+    },
+  },
+  plugins: [],
+};
+export default config;
